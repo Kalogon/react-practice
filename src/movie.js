f5c29222a5ac324649f3a6b1bf6ecb353e6fcbcc
Kalogon <ghlee5084@kaist.ac.kr>
2020-02-10 01:42:25 +0900

    finish-tutorial

--- src/movie.js
+++ src/movie.js
@@ -35,27 +35,51 @@ import './movie.css';
 //     }
 // }
 
-function movie({title,poster}){
+function movie({title,poster,genres,synopsis}){
     return ( 
         <div className="movie">
-            <Movieposter poster = {poster}  />
-            <h1>{title}</h1>
+            <div className="Movie_Columns">
+                <Movieposter poster = {poster}  />
+            </div>
+            <div className="Movie_Columns">
+                <h1>{title}</h1>
+                <div className="Movie_genres">
+                    {genres.map((genre,index)=>{
+                        return <Moviegenre genre={genre} key={index}/>
+                    })}
+                </div>
+                <p className="Movie_Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+           
         </div>
     );
 }
 
-movie.propTypes = {
-    title:PropTypes.string.isRequired,
-    poster:PropTypes.string.isRequired
-}
 
 function Movieposter({poster}){
     return (
         <img src={poster} alt="noimage"></img>
     )
 }
+
+function Moviegenre({genre,key}){
+    return(
+        <span className="Movie_genre" key={key}>{genre}</span>
+    )
+}
+
+movie.propTypes = {
+    title:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired,
+    genres:PropTypes.array.isRequired,
+    synopsis:PropTypes.string.isRequired
+}
 Movieposter.propTypes = {
     poster : PropTypes.string.isRequired
 }
-
+Moviegenre.propTypes = {
+    genre : PropTypes.string.isRequired
+}
 export default movie;
