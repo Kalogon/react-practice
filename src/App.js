e76a5ca3457f7a32ed161ff2e9537770754a0312
Kalogon <ghlee5084@kaist.ac.kr>
2020-02-09 23:04:46 +0900

    loading-state

--- src/App.js
+++ src/App.js
@@ -2,31 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './movie'
 
-const movieArray =[
-  {
-    title:"Matrix",
-    poster:"https://images-na.ssl-images-amazon.com/images/I/71-%2B2B-BJWL._SY679_.jpg"
-  },
-  {
-    title:"Full Metal Jacket",
-    poster:"https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-  },
-  {
-    title:"Oldboy",
-    poster:"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title:"Star Wars",
-    poster:"https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
-  }
-]
 class App extends Component {
+
+  //render: componentWillMount -> render -> componentDidMount
+  //update: componentWillReceiveProps -> shouldComponentUpdate == true -> componentWillUpdate -> render -> componentDidUpdate
+
+  state ={
+    greeting : "Hello" 
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        movieArray : [
+          {
+            title:"Matrix",
+            poster:"https://images-na.ssl-images-amazon.com/images/I/71-%2B2B-BJWL._SY679_.jpg"
+          },
+          {
+            title:"Full Metal Jacket",
+            poster:"https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+          },
+          {
+            title:"Oldboy",
+            poster:"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+          },
+          {
+            title:"Star Wars",
+            poster:"https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+          }
+        ]
+      })
+    },3000)
+  }
+
+  _renderMovies = ()=>{
+    const movies = this.state.movieArray.map((movie,index) =>{
+      return <Movie title={movie.title} poster={movie.poster} key={index}/> 
+    });
+    return movies;
+  }
+
   render(){
+    console.log("render")
     return (
       <div className="App">
-        {movieArray.map((movie,index) =>{
-          return <Movie title={movie.title} poster={movie.poster} key={index}/> 
-        })}
+        {this.state.movieArray ? this._renderMovies() : "loading"}
       </div>
     );
   }
