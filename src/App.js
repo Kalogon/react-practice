bb9757508ebe50dc2a583dd40c37146db9102d55
Kalogon <ghlee5084@kaist.ac.kr>
2020-02-17 14:49:40 +0900

    add-router

--- src/App.js
+++ src/App.js
@@ -1,62 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Movie from './movie'
+import {HashRouter, Route} from 'react-router-dom' 
+import About from './routes/About'
+import Home from './routes/Home'
 
-class App extends Component {
-
-  //render: componentWillMount -> render -> componentDidMount
-  //update: componentWillReceiveProps -> shouldComponentUpdate == true -> componentWillUpdate -> render -> componentDidUpdate
-  state = {}
-
-  componentDidMount(){
-    this._getMovies();
-  }
-
-  _renderMovies = ()=>{
-    console.log("1")
-    console.log(this.state.movieArray);
-    const movies = this.state.movieArray.map((movie) =>{
-      return <Movie 
-        title={movie.title_english} 
-        poster={movie.medium_cover_image} 
-        key={movie.id}
-        genres={movie.genres}
-        synopsis={movie.synopsis}/> 
-    });
-    return movies;
-  }
-
-  _getMovies = async ()=>{
-    const movies = await this._callApi()
-    console.log(movies)
-    this.setState({
-      movieArray:movies
-    })
-    console.log(this.state)
-  }
-
-  _callApi = ()=>{
-    return fetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
-    .then(res=> {
-      return res.json()
-    })
-    .then(json=> {
-      return json.data.movies
-    })
-    .catch(err=>{
-      console.log(err)
-    })
-  }
-
-  render(){
-    console.log("render")
-    return (
-      <div className="App">
-        {this.state.movieArray ? this._renderMovies() : "loading"}
-      </div>
-    );
-  }
-  
+function App(){
+  return ( 
+    <HashRouter>
+      <Route path="/about" exact={true} component={About}/>
+      <Route path="/" exact={true} component={Home}/>
+    </HashRouter>
+  )
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Movie from '../components/movie'
+
+class Home extends Component {
+
+  //render: componentWillMount -> render -> componentDidMount
+  //update: componentWillReceiveProps -> shouldComponentUpdate == true -> componentWillUpdate -> render -> componentDidUpdate
+  state = {}
+
+  componentDidMount(){
+    this._getMovies();
+  }
+
+  _renderMovies = ()=>{
+    console.log("1")
+    console.log(this.state.movieArray);
+    const movies = this.state.movieArray.map((movie) =>{
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}/> 
+    });
+    return movies;
+  }
+
+  _getMovies = async ()=>{
+    const movies = await this._callApi()
+    console.log(movies)
+    this.setState({
+      movieArray:movies
+    })
+    console.log(this.state)
+  }
+
+  _callApi = ()=>{
+    return fetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+    .then(res=> {
+      return res.json()
+    })
+    .then(json=> {
+      return json.data.movies
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  render(){
+    console.log("render")
+    return (
+      <div className="App">
+        {this.state.movieArray ? this._renderMovies() : "loading"}
+      </div>
+    );
+  }
+  
+}
+
+export default Home;
